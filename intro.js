2783ccf8da05ad5e1210a4bc9c82e79cc7dbf7a6
PopanIonut <ionut.popan@yahoo.com>
2019-03-18 21:23:17 +0200

    display skills froms js in html

--- intro.js
+++ intro.js
@@ -62,15 +62,17 @@ function hideAllPages(){
 initMenu();
 
 
-function showSkills(){
-    var skills = ['html','css','js'];
-    console.warn('showSkills',skills);
-    skills.forEach(function(skill, index) {
-        console.info( '#' + (index + 1) + " " + skill);
-    }
-});
+function showSkills() {
+    var skills = ['html', 'css', 'js', 'funny'];
 
-    
+    var htmlSkills = skills.map(function (skill, index) {
+        return '<li>' + skill + '</li>';
+
+    });
+
+    var ul = document.querySelector('#skills-page ul');
+    ul.innerHTML = htmlSkills.join('');
+}
 
 showSkills(); 
 
